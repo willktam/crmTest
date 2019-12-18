337917f3dc41376872efcfc1cb7c4e996a426d0c
William <William@DESKTOP-IPM2U06>
2019-12-18 15:25:03 +1030

    Fixed Leads Map

--- src/generated/java/modules/sales/domain/Opportunity.java
+++ src/generated/java/modules/sales/domain/Opportunity.java
@@ -7,6 +7,7 @@ import javax.xml.bind.annotation.XmlType;
 import modules.customers.domain.Account;
 import modules.customers.domain.ContactDetail;
 import modules.products.domain.ProductInfo;
+import modules.products.domain.ProductPriceList;
 import org.skyve.CORE;
 import org.skyve.domain.messages.DomainException;
 import org.skyve.impl.domain.AbstractPersistentBean;
@@ -17,6 +18,7 @@ import org.skyve.impl.domain.AbstractPersistentBean;
  * @navhas n product 1 ProductInfo
  * @navhas n contact 1 ContactDetail
  * @navhas n account 1 Account
+ * @navhas n priceList 1 ProductPriceList
  * @stereotype "persistent"
  */
 @XmlType
@@ -43,6 +45,18 @@ public class Opportunity extends AbstractPersistentBean {
 	public static final String descriptionPropertyName = "description";
 	/** @hidden */
 	public static final String productPropertyName = "product";
+	/** @hidden */
+	public static final String priceListPropertyName = "priceList";
+	/** @hidden */
+	public static final String amountPropertyName = "amount";
+	/** @hidden */
+	public static final String bulkDiscountPropertyName = "bulkDiscount";
+	/** @hidden */
+	public static final String manualDiscountPropertyName = "manualDiscount";
+	/** @hidden */
+	public static final String taxPropertyName = "tax";
+	/** @hidden */
+	public static final String totalPropertyName = "total";
 
 	/**
 	 * Topic
@@ -72,6 +86,42 @@ public class Opportunity extends AbstractPersistentBean {
 	 * The product of the opportunity
 	 **/
 	private ProductInfo product = null;
+	/**
+	 * Product Price List
+	 * <br/>
+	 * The price list to use for the selected product
+	 **/
+	private ProductPriceList priceList = null;
+	/**
+	 * Amount
+	 * <br/>
+	 * The amount of product for this opportunity
+	 **/
+	private Long amount;
+	/**
+	 * Bulk Discount
+	 * <br/>
+	 * The bulk discount for the inputted amount
+	 **/
+	private Integer bulkDiscount;
+	/**
+	 * Manual Discount
+	 * <br/>
+	 * The amount of manual discount to be applied
+	 **/
+	private Integer manualDiscount;
+	/**
+	 * Tax
+	 * <br/>
+	 * The amount of tax for this amount
+	 **/
+	private Integer tax;
+	/**
+	 * Provisional Total
+	 * <br/>
+	 * The provisional total for this opportunity
+	 **/
+	private Long total;
 
 	@Override
 	@XmlTransient
@@ -204,5 +254,151 @@ public class Opportunity extends AbstractPersistentBean {
 	public void setProduct(ProductInfo product) {
 		preset(productPropertyName, product);
 		this.product = product;
+	}
+
+	/**
+	 * {@link #priceList} accessor.
+	 * @return	The value.
+	 **/
+	public ProductPriceList getPriceList() {
+		return priceList;
+	}
+
+	/**
+	 * {@link #priceList} mutator.
+	 * @param priceList	The new value.
+	 **/
+	@XmlElement
+	public void setPriceList(ProductPriceList priceList) {
+		preset(priceListPropertyName, priceList);
+		this.priceList = priceList;
+	}
+
+	/**
+	 * {@link #amount} accessor.
+	 * @return	The value.
+	 **/
+	public Long getAmount() {
+		return amount;
+	}
+
+	/**
+	 * {@link #amount} mutator.
+	 * @param amount	The new value.
+	 **/
+	@XmlElement
+	public void setAmount(Long amount) {
+		preset(amountPropertyName, amount);
+		this.amount = amount;
+	}
+
+	/**
+	 * {@link #bulkDiscount} accessor.
+	 * @return	The value.
+	 **/
+	public Integer getBulkDiscount() {
+		return bulkDiscount;
+	}
+
+	/**
+	 * {@link #bulkDiscount} mutator.
+	 * @param bulkDiscount	The new value.
+	 **/
+	@XmlElement
+	public void setBulkDiscount(Integer bulkDiscount) {
+		preset(bulkDiscountPropertyName, bulkDiscount);
+		this.bulkDiscount = bulkDiscount;
+	}
+
+	/**
+	 * {@link #manualDiscount} accessor.
+	 * @return	The value.
+	 **/
+	public Integer getManualDiscount() {
+		return manualDiscount;
+	}
+
+	/**
+	 * {@link #manualDiscount} mutator.
+	 * @param manualDiscount	The new value.
+	 **/
+	@XmlElement
+	public void setManualDiscount(Integer manualDiscount) {
+		preset(manualDiscountPropertyName, manualDiscount);
+		this.manualDiscount = manualDiscount;
+	}
+
+	/**
+	 * {@link #tax} accessor.
+	 * @return	The value.
+	 **/
+	public Integer getTax() {
+		return tax;
+	}
+
+	/**
+	 * {@link #tax} mutator.
+	 * @param tax	The new value.
+	 **/
+	@XmlElement
+	public void setTax(Integer tax) {
+		preset(taxPropertyName, tax);
+		this.tax = tax;
+	}
+
+	/**
+	 * {@link #total} accessor.
+	 * @return	The value.
+	 **/
+	public Long getTotal() {
+		return total;
+	}
+
+	/**
+	 * {@link #total} mutator.
+	 * @param total	The new value.
+	 **/
+	@XmlElement
+	public void setTotal(Long total) {
+		preset(totalPropertyName, total);
+		this.total = total;
+	}
+
+	/**
+	 * priceListSelected
+	 *
+	 * @return The condition
+	 */
+	@XmlTransient
+	public boolean isPriceListSelected() {
+		return (priceList != null);
+	}
+
+	/**
+	 * {@link #isPriceListSelected} negation.
+	 *
+	 * @return The negated condition
+	 */
+	public boolean isNotPriceListSelected() {
+		return (! isPriceListSelected());
+	}
+
+	/**
+	 * productSelected
+	 *
+	 * @return The condition
+	 */
+	@XmlTransient
+	public boolean isProductSelected() {
+		return (product != null);
+	}
+
+	/**
+	 * {@link #isProductSelected} negation.
+	 *
+	 * @return The negated condition
+	 */
+	public boolean isNotProductSelected() {
+		return (! isProductSelected());
 	}
 }
